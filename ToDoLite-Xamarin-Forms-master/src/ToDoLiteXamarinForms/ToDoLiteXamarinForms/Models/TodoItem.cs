065e85420cd3d61dcfeb1270beae56050c032836
Pavel Šafránek <pavel_safranek@kb.cz>
2019-01-23 21:38:05 +0100

    Add diagrams to DOCS

---
+++ ToDoLite-Xamarin-Forms-master/src/ToDoLiteXamarinForms/ToDoLiteXamarinForms/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ToDoLiteXamarinForms.Models
+{
+    public class TodoItem : ModelBase
+    {
+        public bool Done { get; set; }
+        
+        public string Details { get; set; }
+        
+        public string TodoListId { get; set; }
+    }
+}
---
+++ ToDoLite-Xamarin-Forms-master/src/ToDoLiteXamarinForms/ToDoLiteXamarinForms/Storage/StorageRepository.Query.cs
@@ -0,0 +1,160 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using ToDoLiteXamarinForms.Models;
+using Couchbase.Lite;
+using ToDoLiteXamarinForms.Helpers;
+using System.Collections.ObjectModel;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace ToDoLiteXamarinForms.Storage
+{
+    public partial class StorageRepository
+    {
+        private void InitializeQuery()
+        {
+            Manager.SharedInstance.GetDatabase(DatabaseName)
+                .GetView("all-docs")
+                .SetMap((doc, emit) =>
+                {
+                    // Create index on '_id' with reference to the document.
+                    emit(doc["_id"], null);
+                }, "ver2");
+
+            Manager.SharedInstance.GetDatabase(DatabaseName)
+                .GetView("query-view")
+                .SetMap((doc, emit) =>
+                {
+                    if (!doc.ContainsKey("doc") || !doc.ContainsKey("doctype"))
+                    {
+                        return;
+                    }
+
+                    var item = doc["doc"] as JContainer;
+
+                    if (item == null)
+                    {
+                        return;
+                    }
+
+                    item.Children()
+                        .Where(p => p is JProperty)
+                        .Cast<JProperty>()
+                        .Select(p => new { name = p.Name, value = p.Value.ToString().ToLower() })
+                        .ToList()
+                        .ForEach(prop =>
+                        {
+
+                            var doctype = doc["doctype"] != null ? doc["doctype"].ToString() : "unknown";
+                            var v = doctype + "::" + prop.name;
+                            emit(prop.value, v);
+                        });
+                }
+                , "query2");
+
+            var query = Manager.SharedInstance.GetDatabase(DatabaseName)
+                .GetView("all-docs")
+                .CreateQuery();
+
+            query.Run()
+            .ToList()
+            .ForEach(row =>
+            {
+                ProcessDocument(row.Document);
+            });
+
+            var liveQuery = Manager.SharedInstance.GetDatabase(DatabaseName)
+                .GetExistingView("all-docs")
+                .CreateQuery()
+                .ToLiveQuery();
+
+            liveQuery.Changed += (object sender, QueryChangeEventArgs e) =>
+            {
+
+                var conflicts = e.Rows
+                    .Where(item => item.Document.ConflictingRevisions.Any())
+                    .Select(item => item.Document)
+                    ;
+
+                foreach (var item in conflicts)
+                {
+                    ConflictItems.Add(item);
+                }
+
+                Debug.WriteLine("liveQuery.Changed " + e.Rows.Count);
+            };
+
+            liveQuery.Start();
+        }
+
+        public List<object> Search(string prefixWord, int limit, Type type)
+        {
+            List<object> list = new List<object>();
+
+            if (string.IsNullOrWhiteSpace(prefixWord))
+            {
+                return list;
+            }
+
+            var query = Manager.SharedInstance.GetDatabase(DatabaseName)
+                .GetView("query-view")
+                .CreateQuery();
+
+            query.IndexUpdateMode = IndexUpdateMode.Before;
+            query.StartKey = prefixWord.ToLower();
+            query.EndKey = query.StartKey.ToString() + '\uEFFF';
+            query.Limit = limit;
+
+            var queryResult =
+                query
+                .Run()
+                .ToList();
+
+            var queryResultFiltered = queryResult;
+            if (type != null)
+            {
+                var filter = string.Format("{0}::", type.Name);
+                queryResultFiltered
+                    .Where(item => item.Value.ToString().StartsWith(filter));
+            }
+
+            foreach (var item in queryResultFiltered)
+            {
+                var doc = (item.Document.UserProperties["doc"]);// as JContainer).ToObject<type>();
+                list.Add(doc);
+            }
+
+            return list;
+        }
+
+        private void ProcessDocument(Document doc)
+        {
+            if (doc.Id.Contains("::"))
+            {
+                var doctype = doc.CurrentRevision.UserProperties["doctype"].ToString();
+
+                if (doctype == typeof(TodoList).Name.ToLower())
+                {
+                    if (!TodoItems.ContainsKey(doc.Id))
+                    {
+                        TodoItems.Add(doc.Id, new ObservableCollection<TodoItem>());
+                    }
+
+                    TodoLists.Update<TodoList>(doc.UserProperties["doc"]);
+                }
+                else if (doctype == typeof(TodoItem).Name.ToLower())
+                {
+                    var todo = Get<TodoItem>(doc.Id);
+
+                    if (!TodoItems.ContainsKey(todo.TodoListId))
+                    {
+                        TodoItems.Add(todo.TodoListId, new ObservableCollection<TodoItem>());
+                    }
+
+                    TodoItems[todo.TodoListId].Update<TodoItem>(todo);
+                }
+            }
+        }
+    }
+}
