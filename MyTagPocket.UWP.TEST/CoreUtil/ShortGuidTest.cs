6bc7a2401298f9ee44b80de19766e5061202c9a3
Pavel Šafránek <psaf01@gmail.com>
2020-02-05 17:10:19 +0100

    Unit test ShortGuid

--- MyTagPocket.UWP.TEST/CoreUtil/ShortGuidTest.cs
+++ MyTagPocket.UWP.TEST/CoreUtil/ShortGuidTest.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MyTagPocket.CoreUtil;
+using System;
+using Xunit;
 
 namespace MyTagPocket.UWP.TEST.CoreUtil
 {
   public class ShortGuidTest
   {
+    /// <summary>
+    /// 
+    /// </summary>
+    [Fact]
+    public void GuidToShortTest()
+    {
+      Guid guidTest = Guid.Parse("8f055c81-54b6-4aaf-845d-aaeee4e0dc19");
+      ShortGuid shortGuidTest = new ShortGuid(guidTest);
+      var shortGuid = shortGuidTest.Guid;
+      Assert.Equal(guidTest, shortGuid);
+      ShortGuid shortGuidConvert = new ShortGuid("gVwFj7ZUr0qEXaru5ODcGQ");
+      Assert.Equal(guidTest, shortGuidConvert.Guid);
+    }
   }
 }
